1bf3082da6cfaec126c476d1b52374afd0ffd251
DanielBlsn <41128787+DanielBlsn@users.noreply.github.com>
2020-10-28 20:31:12 +0000

    Add files via upload
    Updated navbar

--- js/navbar.js
+++ js/navbar.js
@@ -11,4 +11,23 @@ for (var i = 0; i < navItems.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
+}
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("custom-navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 } 
