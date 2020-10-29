0b328332b078e45cb4f2407e5a7a050c87d590b5
DanielBlsn <41128787+DanielBlsn@users.noreply.github.com>
2020-10-29 19:36:02 +0000

    Add files via upload
    fixed small screen navbar

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,7 @@ var navItems = navContainer.getElementsByClassName("navbar-item");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < navItems.length; i++) {
-  navItems[i].addEventListener("click", function() {
+  navItems[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
@@ -15,7 +15,7 @@ for (var i = 0; i < navItems.length; i++) {
 
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 // Get the navbar
 var navbar = document.getElementById("custom-navbar");
@@ -29,4 +29,34 @@ function myFunction() {
   } else {
     navbar.className = "pre-sticky";
   }
-} 
+}
+
+function openNav() {
+  document.getElementById("menu-item-1").style.visibility = "visible";
+  document.getElementById("menu-item-2").style.visibility = "visible";
+  document.getElementById("menu-item-3").style.visibility = "visible";
+  document.getElementById("menu-item-4").style.visibility = "visible";
+  document.getElementById("custom-navbar").style.height = "auto";
+}
+
+function closeNav() {
+  if (window.innerWidth < 1120) {
+    document.getElementById("menu-item-1").style.visibility = "hidden";
+    document.getElementById("menu-item-2").style.visibility = "hidden";
+    document.getElementById("menu-item-3").style.visibility = "hidden";
+    document.getElementById("menu-item-4").style.visibility = "hidden";
+    document.getElementById("custom-navbar").style.height = document.getElementById("menu-item-1").style.height;
+  }
+}
+
+// When going from small screen navbar to large screen navbar the visibility will be set to hidden
+// Make all navbar elements visible when screen width is bigger than 1120
+window.addEventListener('resize', makeVisibleNavbar);
+function makeVisibleNavbar() {
+  if (window.innerWidth > 1120) {
+    openNav();
+  }
+  else {
+    closeNav();
+  }
+}
