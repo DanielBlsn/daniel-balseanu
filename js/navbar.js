a00fe162a1c78ec6eeb93c760e740eaca6c8d5f1
DanielBlsn <41128787+DanielBlsn@users.noreply.github.com>
2020-11-01 20:36:32 +0000

    Add files via upload
    fixed navbar positioning

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,12 @@
 // When the user scrolls the page, execute the sticky navbar function 
 window.onscroll = function () { stickyNavbar(); highlightNavbarElement(); };
 
+// Get the navbar
+var navbar = document.getElementById("custom-navbar");
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
 function stickyNavbar() {
-
-  // Get the navbar
-  var navbar = document.getElementById("custom-navbar");
-
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
 
   // Add the sticky class to the navbar when you reach its scroll position.
   if (window.pageYOffset >= sticky) {
